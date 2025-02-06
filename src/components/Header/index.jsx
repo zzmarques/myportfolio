5899a63d2e2229a1420dd7374ff1a3f497219898
zzmarques <walacemarqs@gmail.com>
2025-02-06 13:40:23 -0300

    add react icons, e add estrutura do header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,29 @@
-import "../../sass/components/Header.sass"
+import "../../sass/components/Header.sass";
+import  img from "../../../public/assets/imgs/logo.png";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const Header = () => {
     return (
-        <header>
-            <h1>Olá</h1>
+        <header className="header">
+            <div className="container-logo">
+                <img src={img} alt="logo" />
+            </div>
+
+            <nav className="navbar">
+                <div className="navbar__btn-menu">
+                    <RxHamburgerMenu />
+                </div>
+
+                <ul className="navbar__ul">
+                    <li><a href="#">Início</a></li>
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">Habilidades</a></li>
+                    <li><a href="#">Projetos</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+            </nav>
         </header>
     )
 }
 
-export default Header
+export default Header;
