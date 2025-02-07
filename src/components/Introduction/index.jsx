b55c917dd231e8c32eefed9662f8191dff8c9681
zzmarques <walacemarqs@gmail.com>
2025-02-07 13:40:26 -0300

    css container-introduction fenalizado

--- src/components/Introduction/index.jsx
+++ src/components/Introduction/index.jsx
@@ -1,21 +1,30 @@
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 import img from '../../../public/assets/imgs/home-main.svg';
+import "../../sass/components/Introduction.sass";
+
 
 const Introduction = () => {
 
     return (
         <section className="container-introduction">
+
             <div className="container-texts">
+
                 <div className="texts__title">
+
                     <span className="title">Olá!👋🏻</span>
                     <span className="title">Eu sou <span className="destaque">Walace Marques</span></span>
                     <span className="subtitle">Desenvolvedor Front-End</span>
+                    
                 </div>
+
                 <div className="social-midia">
                     <FaLinkedin />
                     <FaGithub />
                 </div>
+
             </div>
+
             <div className="container-img">
                 <img src={img} alt="home hero" />
             </div>
