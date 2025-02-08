779b55998272a485babde87f8622c985394b413f
zzmarques <walacemarqs@gmail.com>
2025-02-08 08:02:43 -0300

    criando estrutura HTML do componente AboutMe

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -1,8 +1,21 @@
+import img from "../../../public/assets/imgs/about.png"
+
 
 const AboutMe = () => {
+
     return (
         <section>
-            <h1>Ola</h1>
+            <div>
+                <p>
+                    Sou um desenvolvedor front-end com a ambição de me tornar um desenvolvedor fullstack. Atualmente, estou concentrado em me especializar no front-end, onde posso aplicar e aperfeiçoar minhas habilidades na criação de interfaces web intuitivas e funcionais. Além dos projetos pessoais, também tenho experiência como freelancer, onde enfrentei e superei diversos desafios, sempre buscando soluções criativas e eficazes.
+                </p>
+                <p>
+                    Meu compromisso com o aprendizado contínuo me motiva a buscar novos projetos e desafios que expandam minhas competências e explorem novas tecnologias. Cada projeto é uma oportunidade valiosa para fortalecer minhas habilidades. Acredito que dedicação e aprendizado constante são essenciais para alcançar meus objetivos como desenvolvedor fullstack. Estou sempre em busca de novas oportunidades para crescer profissionalmente e contribuir com soluções inovadoras no desenvolvimento de interfaces web.
+                </p>
+            </div>
+            <div>
+                <img src={img} alt="about img" />
+            </div>
         </section>
     )
 }
