7be9eab63b123b43c468267ed7aabb164b69de9e
zzmarques <walacemarqs@gmail.com>
2025-02-10 12:59:24 -0300

    criando estrutura html do componente Skills

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,10 +1,36 @@
+import Title from "../Title";
+import { SiReact, SiJavascript, SiHtml5, SiCss3, SiBootstrap, SiGit } from "react-icons/si";
+import { FaSass } from "react-icons/fa6";
 
 
 const Skills = () => {
     return (
-        <>
-            <h1>Skills</h1>
-        </>
+        <section>
+            <Title>Habilidades</Title>
+            <div>
+                <div>
+                    <SiReact/>
+                </div>
+                <div>
+                    <SiJavascript/>
+                </div>
+                <div>
+                    <SiHtml5/>
+                </div>
+                <div>
+                    <SiCss3/>
+                </div>
+                <div>
+                    <FaSass/>
+                </div>
+                <div>
+                    <SiBootstrap/>
+                </div>
+                <div>
+                    <SiGit/>
+                </div>
+            </div>
+        </section>
     )
 }
 
