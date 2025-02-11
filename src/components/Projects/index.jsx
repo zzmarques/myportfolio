5ae20f47d85ab0743e99bfdc2b0fb14e879f9777
zzmarques <walacemarqs@gmail.com>
2025-02-11 13:28:22 -0300

    add classes nas tag do componente Projects

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,24 +1,34 @@
+import { SiReact, SiJavascript, SiHtml5, SiCss3, SiBootstrap, SiGit } from "react-icons/si";
+import { FaSass } from "react-icons/fa6";
 import Title from "../Title";
-
+import img from "../../../public/assets/imgs/imagem-branca.png"
 
 const Projects = () => {
     return (
-        <section>
+        <section className="container-projects">
             <Title>Projetos</Title>
-            <div>
-                <div>
-                    <div>
-                        <img src="" alt="Foto Projeto"/>
+            <div className="container-cards-projects">
+
+                <div className="cards">
+
+                    <div className="container-img">
+                        <img src={img} alt="Foto Projeto"/>
                     </div>
-                    <div>
-                        <div>
-                            <span></span>
+
+                    <div className="">
+                        <div className="title">
+                            <span>Teste</span>
                         </div>
-                        <div>
-                            
+                        <div className="tecs">
+                            <SiReact/>
+                            <SiJavascript/>
+                            <FaSass/>
+                            <SiBootstrap/>
                         </div>
                     </div>
+
                 </div>
+
             </div>
         </section>
     )
