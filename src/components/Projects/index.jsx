bca3a9bf9075c19b28e5df85ac52ebd1b26102f4
zzmarques <walacemarqs@gmail.com>
2025-02-11 14:27:49 -0300

    add css do container-projects

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -2,6 +2,8 @@ import { SiReact, SiJavascript, SiHtml5, SiCss3, SiBootstrap, SiGit } from "reac
 import { FaSass } from "react-icons/fa6";
 import Title from "../Title";
 import img from "../../../public/assets/imgs/imagem-branca.png"
+import "../../sass/components/_Projects.sass";
+
 
 const Projects = () => {
     return (
@@ -15,15 +17,175 @@ const Projects = () => {
                         <img src={img} alt="Foto Projeto"/>
                     </div>
 
-                    <div className="">
+                    <div className="cabecalho">
                         <div className="title">
                             <span>Teste</span>
                         </div>
                         <div className="tecs">
-                            <SiReact/>
-                            <SiJavascript/>
-                            <FaSass/>
-                            <SiBootstrap/>
+                            <SiReact className="icons"/>
+                            <SiJavascript className="icons"/>
+                            <FaSass className="icons"/>
+                            <SiBootstrap className="icons"/>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="cards">
+
+                    <div className="container-img">
+                        <img src={img} alt="Foto Projeto"/>
+                    </div>
+
+                    <div className="cabecalho">
+                        <div className="title">
+                            <span>Teste</span>
+                        </div>
+                        <div className="tecs">
+                            <SiReact className="icons"/>
+                            <SiJavascript className="icons"/>
+                            <FaSass className="icons"/>
+                            <SiBootstrap className="icons"/>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="cards">
+
+                    <div className="container-img">
+                        <img src={img} alt="Foto Projeto"/>
+                    </div>
+
+                    <div className="cabecalho">
+                        <div className="title">
+                            <span>Teste</span>
+                        </div>
+                        <div className="tecs">
+                            <SiReact className="icons"/>
+                            <SiJavascript className="icons"/>
+                            <FaSass className="icons"/>
+                            <SiBootstrap className="icons"/>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="cards">
+
+                    <div className="container-img">
+                        <img src={img} alt="Foto Projeto"/>
+                    </div>
+
+                    <div className="cabecalho">
+                        <div className="title">
+                            <span>Teste</span>
+                        </div>
+                        <div className="tecs">
+                            <SiReact className="icons"/>
+                            <SiJavascript className="icons"/>
+                            <FaSass className="icons"/>
+                            <SiBootstrap className="icons"/>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="cards">
+
+                    <div className="container-img">
+                        <img src={img} alt="Foto Projeto"/>
+                    </div>
+
+                    <div className="cabecalho">
+                        <div className="title">
+                            <span>Teste</span>
+                        </div>
+                        <div className="tecs">
+                            <SiReact className="icons"/>
+                            <SiJavascript className="icons"/>
+                            <FaSass className="icons"/>
+                            <SiBootstrap className="icons"/>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="cards">
+
+                    <div className="container-img">
+                        <img src={img} alt="Foto Projeto"/>
+                    </div>
+
+                    <div className="cabecalho">
+                        <div className="title">
+                            <span>Teste</span>
+                        </div>
+                        <div className="tecs">
+                            <SiReact className="icons"/>
+                            <SiJavascript className="icons"/>
+                            <FaSass className="icons"/>
+                            <SiBootstrap className="icons"/>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="cards">
+
+                    <div className="container-img">
+                        <img src={img} alt="Foto Projeto"/>
+                    </div>
+
+                    <div className="cabecalho">
+                        <div className="title">
+                            <span>Teste</span>
+                        </div>
+                        <div className="tecs">
+                            <SiReact className="icons"/>
+                            <SiJavascript className="icons"/>
+                            <FaSass className="icons"/>
+                            <SiBootstrap className="icons"/>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="cards">
+
+                    <div className="container-img">
+                        <img src={img} alt="Foto Projeto"/>
+                    </div>
+
+                    <div className="cabecalho">
+                        <div className="title">
+                            <span>Teste</span>
+                        </div>
+                        <div className="tecs">
+                            <SiReact className="icons"/>
+                            <SiJavascript className="icons"/>
+                            <FaSass className="icons"/>
+                            <SiBootstrap className="icons"/>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="cards">
+
+                    <div className="container-img">
+                        <img src={img} alt="Foto Projeto"/>
+                    </div>
+
+                    <div className="cabecalho">
+                        <div className="title">
+                            <span>Teste</span>
+                        </div>
+                        <div className="tecs">
+                            <SiReact className="icons"/>
+                            <SiJavascript className="icons"/>
+                            <FaSass className="icons"/>
+                            <SiBootstrap className="icons"/>
                         </div>
                     </div>
 
