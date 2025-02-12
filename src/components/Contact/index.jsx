99a6c8ebe3e5c19821c5f8d8f64a83ec5f9d0359
zzmarques <walacemarqs@gmail.com>
2025-02-12 14:04:37 -0300

    add estrutura html do componente Contact

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,10 +1,43 @@
-
+import Title from "../Title";
 
 const Contact = () => {
     return (
-        <>
-            <h1>Ola</h1>
-        </>
+        <section>
+            <Title>Contato</Title>
+            <div>
+
+                <div>
+
+                    <div>
+                        <span>E-mail</span>
+                    </div>
+                    <span>walacemarqs@gmail.com</span>
+                </div>
+
+                <div>
+
+                    <div>
+                        <span>WhatsApp</span>
+                    </div>
+                    <span>(32) 998674766</span>
+                </div>
+
+                <div>
+                    <div>
+                        <span>LinkedIn</span>
+                    </div>
+                    <span>@walacemarques</span>
+                </div>
+
+                <div>
+                    <div>
+                        <span>Github</span>
+                    </div>
+                    <span>@zzmarques</span>
+                </div>
+
+            </div>
+        </section>
     )
 }
 
