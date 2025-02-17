42a6951df4c4d453a4827045d433698b96f2ee03
zzmarques <walacemarqs@gmail.com>
2025-02-17 13:18:53 -0300

    add container de text no componente skills

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -9,6 +9,11 @@ const Skills = () => {
     return (
         <section className="container-skills">
             <Title>Habilidades</Title>
+            <div className="container-text-skills">
+                <p>
+                    Sou um desenvolvedor web com habilidades sólidas em HTML5, CSS3, JavaScript, React, Sass e Bootstrap. Tenho experiência em criar soluções web inovadoras que são otimizadas para uma variedade de dispositivos. Meu foco está sempre em manter-me atualizado com as últimas tendências e melhores práticas do setor para garantir que meus projetos sejam eficientes e de ponta. Além de aperfeiçoar minhas habilidades atuais, estou sempre em busca de novos conhecimentos em ferramentas e tecnologias emergentes para ampliar meu repertório. Estou comprometido com o aprendizado contínuo e acredito que diversificar minhas habilidades é essencial para me tornar um desenvolvedor cada vez mais completo e versátil.
+                </p>
+            </div>
             <div className="container-cards-skills">
 
                 <div className="cards-skills">
