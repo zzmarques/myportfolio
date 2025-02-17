f4bc22c6f4f042d01ba0edb9bdf0c40e426c36f4
zzmarques <walacemarqs@gmail.com>
2025-02-17 13:39:01 -0300

    finalizando css do container skills content no componente skills

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -9,40 +9,45 @@ const Skills = () => {
     return (
         <section className="container-skills">
             <Title>Habilidades</Title>
-            <div className="container-text-skills">
-                <p>
-                    Sou um desenvolvedor web com habilidades sólidas em <span>HTML5</span>, <span>CSS3</span>, <span>JavaScript</span>, <span>React</span>, <span>Sass</span> e <span>Bootstrap</span>. Tenho experiência em criar soluções web inovadoras que são otimizadas para uma variedade de dispositivos. Meu foco está sempre em manter-me atualizado com as últimas tendências e melhores práticas do setor para garantir que meus projetos sejam eficientes e de ponta. Além de aperfeiçoar minhas habilidades atuais, estou sempre em busca de novos conhecimentos em ferramentas e tecnologias emergentes para ampliar meu repertório. Estou comprometido com o aprendizado contínuo e acredito que diversificar minhas habilidades é essencial para me tornar um desenvolvedor cada vez mais completo e versátil.
-                </p>
-            </div>
-            <div className="container-cards-skills">
+            <div className="container-skills-content">
 
-                <div className="cards-skills">
-                    <SiReact className="hab"/>
+                <div className="container-text-skills">
+                    <p>
+                        Sou um desenvolvedor web com habilidades sólidas em <span>HTML5</span>, <span>CSS3</span>, <span>JavaScript</span>, <span>React</span>, <span>Sass</span> e <span>Bootstrap</span>. Tenho experiência em criar soluções web inovadoras que são otimizadas para uma variedade de dispositivos. Meu foco está sempre em manter-me atualizado com as últimas tendências e melhores práticas do setor para garantir que meus projetos sejam eficientes e de ponta. Além de aperfeiçoar minhas habilidades atuais, estou sempre em busca de novos conhecimentos em ferramentas e tecnologias emergentes para ampliar meu repertório. Estou comprometido com o aprendizado contínuo e acredito que diversificar minhas habilidades é essencial para me tornar um desenvolvedor cada vez mais completo e versátil.
+                    </p>
                 </div>
 
-                <div className="cards-skills">
-                    <SiJavascript className="hab"/>
-                </div>
+                <div className="container-cards-skills">
 
-                <div className="cards-skills">
-                    <SiHtml5 className="hab"/>
-                </div>
+                    <div className="cards-skills">
+                        <SiReact className="hab"/>
+                    </div>
 
-                <div className="cards-skills">
-                    <SiCss3 className="hab"/>
-                </div>
+                    <div className="cards-skills">
+                        <SiJavascript className="hab"/>
+                    </div>
 
-                <div className="cards-skills">
-                    <FaSass className="hab"/>
-                </div>
+                    <div className="cards-skills">
+                        <SiHtml5 className="hab"/>
+                    </div>
 
-                <div className="cards-skills">
-                    <SiBootstrap className="hab"/>
-                </div>
+                    <div className="cards-skills">
+                        <SiCss3 className="hab"/>
+                    </div>
 
-                <div className="cards-skills">
-                    <SiGit className="hab"/>
+                    <div className="cards-skills">
+                        <FaSass className="hab"/>
+                    </div>
+
+                    <div className="cards-skills">
+                        <SiBootstrap className="hab"/>
+                    </div>
+
+                    <div className="cards-skills">
+                        <SiGit className="hab"/>
+                    </div>
                 </div>
+                
             </div>
         </section>
     )
