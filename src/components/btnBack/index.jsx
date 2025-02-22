5fb49217c669c5d87a2aa00eb1cd73bf9761f998
zzmarques <walacemarqs@gmail.com>
2025-02-22 08:21:04 -0300

    att estrutura html do componente btnBack

--- src/components/btnBack/index.jsx
+++ src/components/btnBack/index.jsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
+import { FaArrowCircleLeft } from "react-icons/fa";
 
 
 const btnBack = () => {
     return (
         <Link to="/">
-            <button>Voltar</button>
+            <button>
+                <FaArrowCircleLeft />
+                <span>Voltar</span>
+            </button>
         </Link>
     )
 }
