a25317f215956d13f82c3207a922c30c2e623e58
zzmarques <walacemarqs@gmail.com>
2025-02-22 08:17:36 -0300

    criando componente btnBack

---
+++ src/components/btnBack/index.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+
+const btnBack = () => {
+    return (
+        <Link to="/">
+            <button>Voltar</button>
+        </Link>
+    )
+}
+
+export default btnBack;
