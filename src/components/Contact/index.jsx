3eea361c522d9828aec61b1c8edd6a5b15e9c503
zzmarques <walacemarqs@gmail.com>
2025-02-23 08:42:05 -0300

    add link no componente contact

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -22,42 +22,51 @@ const Contact = () => {
             </Title>
             <div className="container-midia-social">
 
-                <div className="cards-midia-social">
+                
+                <a href="mailto:walacemarqs@gmail.com" target="_blank">
+                    <div className="cards-midia-social">
 
-                    <div className="container-icon">
-                        <MdOutlineEmail  className="icon"/>
-                        <span className="title">E-mail</span>
+                        <div className="container-icon">
+                            <MdOutlineEmail  className="icon"/>
+                            <span className="title">E-mail</span>
+                        </div>
+                        <span className="subtitle">walacemarqs@gmail.com</span>
                     </div>
-                    <span className="subtitle">walacemarqs@gmail.com</span>
-                </div>
+                </a>
 
-                <div className="cards-midia-social">
+                <a href="https://wa.me/5532998674766" target="_blank">
+                    <div className="cards-midia-social">
 
-                    <div className="container-icon">
-                        <FaWhatsapp className="icon"/>
-                        <span className="title">WhatsApp</span>
+                        <div className="container-icon">
+                            <FaWhatsapp className="icon"/>
+                            <span className="title">WhatsApp</span>
+                        </div>
+                        <span className="subtitle">(32) 998674766</span>
                     </div>
-                    <span className="subtitle">(32) 998674766</span>
-                </div>
+                </a>
 
-                <div className="cards-midia-social">
+                <a href="https://www.linkedin.com/in/walacemarques/" target="_blank">
+                    <div className="cards-midia-social">
 
-                    <div className="container-icon">
-                        <FaLinkedin className="icon"/>
-                        <span className="title">LinkedIn</span>
+                        <div className="container-icon">
+                            <FaLinkedin className="icon"/>
+                            <span className="title">LinkedIn</span>
+                        </div>
+                        <span className="subtitle">@walacemarques</span>
                     </div>
-                    <span className="subtitle">@walacemarques</span>
-                </div>
+                </a>
 
-                <div className="cards-midia-social">
+                <a href="https://github.com/zzmarques" target="_blank">
 
-                    <div className="container-icon">
-                        <FaGithub className="icon"/>
-                        <span className="title">Github</span>
+                    <div className="cards-midia-social">
+
+                        <div className="container-icon">
+                            <FaGithub className="icon"/>
+                            <span className="title">Github</span>
+                        </div>
+                        <span className="subtitle">@zzmarques</span>
                     </div>
-                    <span className="subtitle">@zzmarques</span>
-                </div>
-
+                </a>
             </div>
         </section>
     )
