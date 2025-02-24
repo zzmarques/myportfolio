13ae846b1cc32de9958db3e1431f9517708258d8
zzmarques <walacemarqs@gmail.com>
2025-02-24 13:44:13 -0300

    implementando funcionalidade do menu hamburger

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,36 +2,56 @@ import "../../sass/components/_Header.sass";
 import  img from "../../../public/assets/imgs/logo.png";
 import { IoMdMenu, IoMdClose } from "react-icons/io";
 import { Link } from "react-scroll";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+
+    const [ toggle, setToggle ] = useState(false);
+
+    const showMenu = () => {
+        setToggle(!toggle);
+    };
+
+    useEffect(() => {
+        document.body.style.overflow = toggle ? "hidden" : "auto";
+    }, [toggle]);
+
+    const closeMenu = () => {
+        setToggle(false)
+    }
+
     return (
         <header className="header">
             <div className="container-logo">
                 <img src={img} alt="logo" />
             </div>
 
-            <nav className="navbar">
-                <div className="navbar__btn-menu">
-                    <IoMdMenu className="menu-icon"/>
+            <nav className={`navbar ${toggle ? "active" : ""}`}>
+                <div 
+                    className="navbar__btn-menu"
+                    onClick={showMenu}
+                
+                >
+                    {toggle ? <IoMdClose className="menu-icon"/>  : <IoMdMenu className="menu-icon"/> }
+                    
                 </div>
 
                 <div className="container-ul">
                     <ul className="navbar__ul">
-                        <Link to="/" smooth={true} duration={500}>
+                        <Link to="/" smooth={true} duration={500} onClick={closeMenu}>
                             <li>Início</li>
                         </Link>
-
-                        <Link to="about" smooth={true} duration={500}>
+                        <Link to="about" smooth={true} duration={500} onClick={closeMenu}>
                             <li>Sobre</li>
                         </Link>
 
-                        <Link to="skills" smooth={true} duration={500}>
+                        <Link to="skills" smooth={true} duration={500} onClick={closeMenu}>
                             <li>Habilidades</li>
                         </Link>
-                        <Link to="project" smooth={true} duration={500}>
+                        <Link to="project" smooth={true} duration={500} onClick={closeMenu}>
                             <li>Projetos</li>
                         </Link>
-                        <Link to="contact" smooth={true} duration={500}>
+                        <Link to="contact" smooth={true} duration={500} onClick={closeMenu}>
                             <li>Contato</li>
                         </Link>
                     </ul>
