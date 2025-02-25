192adf915bc41362477230d394543d8a580a7c65
zzmarques <walacemarqs@gmail.com>
2025-02-25 14:04:55 -0300

    criando componente ParticlesTs

---
+++ src/components/ParticlesTs/index.jsx
@@ -0,0 +1,75 @@
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
+
+const ParticlesTs = () => {
+    const [init, setInit] = useState(false);
+
+    useEffect(() => {
+        initParticlesEngine(async (engine) => {
+            await loadSlim(engine);
+        }).then(() => {
+            setInit(true);
+        });
+    }, []);
+
+    const particlesLoaded = (container) => {
+        console.log(container);
+    };
+
+    const options = useMemo(
+        () => ({
+            fullScreen: {
+                enable: true,
+                zIndex: 0,
+            },
+            detectRetina: true,
+            fpsLimit: 120,
+            interactivity: {
+                events: {
+                    resize: { enable: true },
+                },
+                modes: {
+                    bubble: { distance: 250, duration: 2, opacity: 0, size: 0 },
+                    repulse: { distance: 400, duration: 0.4 },
+                },
+            },
+            particles: {
+                color: { value: "#ffffff" },
+                move: {
+                    enable: true,
+                    speed: { min: 0.1, max: 1 },
+                    direction: "none",
+                    outModes: { default: "out" },
+                },
+                number: {
+                    density: { enable: true, width: 1920, height: 1080 },
+                    value: 160,
+                },
+                opacity: {
+                    value: { min: 0.1, max: 1 },
+                    animation: { enable: true, speed: 1, startValue: "random" },
+                },
+                shape: { type: "circle" },
+                size: { value: { min: 1, max: 3 } },
+            },
+            pauseOnBlur: true,
+            pauseOnOutsideViewport: true,
+        }),
+        []
+    );
+
+    if (init) {
+        return (
+            <Particles
+                id="tsparticles"
+                particlesLoaded={particlesLoaded}
+                options={options}
+            />
+        );
+    }
+
+    return <></>;
+};
+
+export default ParticlesTs;
