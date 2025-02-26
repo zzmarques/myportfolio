cdd558dc87a0132d076695d5ad9335c7a7fdaa48
zzmarques <walacemarqs@gmail.com>
2025-02-26 16:49:22 -0300

    removendo biblioteca gsap e removendo animação da gsap no header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,5 @@
-import React, { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-scroll";
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { IoMdMenu, IoMdClose } from "react-icons/io";
 import img from "../../../public/assets/imgs/logo.png";
 import "../../sass/components/_Header.sass";
@@ -9,30 +7,6 @@ import "../../sass/components/_Header.sass";
 
 
 const Header = () => {
-
-    const headerRef = useRef(null);
-
-    useEffect(() => {
-        gsap.registerPlugin(ScrollTrigger);
-
-        const showAnim = gsap.from(headerRef.current, {
-            yPercent: -100,
-            paused: true,
-            duration: 0.2,
-        }).progress(1);
-
-        ScrollTrigger.create({
-            start: 'top top',
-            end: 'max',
-            onUpdate: (self) => {
-                self.direction === -1 ? showAnim.play() : showAnim.reverse();
-            },
-        });
-
-        return () => {
-            ScrollTrigger.getAll().forEach(trigger => trigger.kill());
-        };
-    }, []);
 
     const [toggle, setToggle] = useState(false);
 
@@ -66,20 +40,20 @@ const Header = () => {
 
                 <div className="container-ul">
                     <ul className="navbar__ul">
-                        <Link to="/" smooth={true} duration={500} onClick={closeMenu}>
+                        <Link to="/" smooth={true} duration={100} onClick={closeMenu}>
                             <li>Início</li>
                         </Link>
-                        <Link to="about" smooth={true} duration={500} onClick={closeMenu}>
+                        <Link to="about" smooth={true} duration={100} onClick={closeMenu}>
                             <li>Sobre</li>
                         </Link>
 
-                        <Link to="skills" smooth={true} duration={500} onClick={closeMenu}>
+                        <Link to="skills" smooth={true} duration={100} onClick={closeMenu}>
                             <li>Habilidades</li>
                         </Link>
-                        <Link to="project" smooth={true} duration={500} onClick={closeMenu}>
+                        <Link to="project" smooth={true} duration={100} onClick={closeMenu}>
                             <li>Projetos</li>
                         </Link>
-                        <Link to="contact" smooth={true} duration={500} onClick={closeMenu}>
+                        <Link to="contact" smooth={true} duration={100} onClick={closeMenu}>
                             <li>Contato</li>
                         </Link>
                     </ul>
